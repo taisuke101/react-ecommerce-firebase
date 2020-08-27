ad2f59c0c14565fb1396d05da888f12d49dbecdb
taisuke <uwattotaitai@gmail.com>
2020-08-27 16:04:34 +0900

    ドロップダウンリスト、カートセレクタ-の追加

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component'
+import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart-selectors';
 
 import './cart-dropdown.styles.scss'
 
 
-const Cart = () => {
+const CartDropdown = ({ cartItems }) => {
     return (
         <div className='cart-dropdown'>
-            <div className='cart-items' />
+            <div className='cart-items' >
+                {cartItems.map(cartItem => (
+                    <CartItem key={cartItem.id} item={cartItem} />
+                ))}
+            </div>
             <CustomButton>Checkout</CustomButton>
         </div>
     )
 }
 
-export default Cart
+const mapStateToProps = state => ({
+    cartItems: selectCartItems(state)
+})
+
+export default connect(mapStateToProps)(CartDropdown);
